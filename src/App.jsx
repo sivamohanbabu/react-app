114ae8cc3a1bf148c0edd7619bc564988ab8f0db
sivamohanbabu <sivamohanbabu@gmail.com>
2024-11-21 11:47:37 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div style={{ color: "red" }}>Hello React app </div>;
+  return <div style={{ color: "green" }}>Hello React app1 </div>;
 }
 
 export default App;
